873e762af4a4f8f0c1c7450967cf8523434fb0fc
CannelleRb <cannelle.ruschbonte@gmail.com>
2017-06-28 13:06:22 +0200

    [StringBuilder update] converted Strings into StringBuilders in Main class

--- src/productSorter/Main.java
+++ src/productSorter/Main.java
@@ -23,30 +23,30 @@ public class Main {
 	  public static void main (String[] args){
 		 // Mise en place d'un tableau d'exemples  
 		   
-	     String[] test = {"163841689525773",
-	                      "16384168952577142537685487999564612313276566679584623131313433522767655243425433461516",
-	                      "11111111111",
-	                      "",
-	                      "",
-	                      ""/*,
-	                      ""*/};
+	     	 StringBuilder[] test = {new StringBuilder("163841689525773"),
+	    		 new StringBuilder("16384168952577142537685487999564612313276566679584623131313433522767655243425433461516"),
+	    		 new StringBuilder("11111111111"),
+	    		 new StringBuilder(),
+	    		 new StringBuilder(),
+	    		 new StringBuilder(),
+	    		 new StringBuilder()};
 	     
-		 for (int i = 0; i < 100000/*0*/; i++) {
+		 for (int i = 0; i < 1000000; i++) {
 			 int temp = (int)(Math.random()*10);
 			 if (temp == 0) {
 				 temp = temp + 1;
 			 }
 			 
 			 if (i < 5000) {
-				 test[3] += temp;
+				 test[3].append(temp);
 			 }
 			 if (i < 10000) {
-				 test[4] += temp;
+				 test[4].append(temp);
 			 }
 			 if (i < 100000) {
-				 test[5] += temp;
+				 test[5].append(temp);
 			 }
-			 //test[6] += temp;
+			 test[6].append(temp);
 		 }
 	     
 		 // Début du test
